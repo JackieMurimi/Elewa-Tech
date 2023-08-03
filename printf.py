c8ae9add135c07e67caaf7fdbdd332040b543c03
JackieMurimi <murimijacque@gmail.com>
2023-08-03 21:32:45 +0300

    first commit

---
+++ printf.py
@@ -0,0 +1,6 @@
+#Writing my first hello world
+print("Hello World")
+print("I am Jackie")
+print("I am 30 years old")
+print("I am excited about the next 100 days as there's so much to learn about python")
+#i'm really enjoying this
---
+++ variable.py
@@ -0,0 +1,26 @@
+#understanding variables in python
+#I will be assigning different variables to different values
+
+#variable representation
+first_var = "My mental health"
+second_var = "Famiy"
+
+#print(first_var)
+#print(second_var)
+
+#second representation
+motivation_1 = "I want to be a good programmer"
+motivation_2 = "I love being the best at what i do"
+
+#print(var_1)
+#print(var_2)
+
+#third representation
+first_conviction = "I believe that there's God"
+second_conviction = "I also believe that there's power in manifestation"
+
+print(first_conviction)
+print(second_conviction)
+
+
+
