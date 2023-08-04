4bb03c4eeb995c3be6b0233155a2a7ca440b4712
JackieMurimi <murimijacque@gmail.com>
2023-08-04 21:37:44 +0300

    learning python arithmetic Arithmetic_ops

---
+++ Arithmetic_ops.py
@@ -0,0 +1,44 @@
+#a demo of basic arithmetic operators in python
+#addition
+var_1 = 10
+var_2 = 4
+#code starts here
+var_3 = var_1 + var_2
+print(var_3)
+print(var_1 + var_2)
+
+#subtraction
+var_4 = var_1 - var_2
+print(var_4)
+print(var_1 - var_2)
+
+#multplication
+var_5 = var_1 * var_2
+print(var_5)
+print(var_1 * var_2)
+
+#division
+var_6 = var_1 / var_2
+print(var_6)
+print(var_1 / var_2)
+
+#modulus
+var_7 = var_1 % var_2
+print(var_7)
+print(var_1 % var_2)
+
+#division flow
+var_8 = var_1 // var_2
+print(var_8)
+print(var_1 // var_2)
+
+#power
+var_9 = var_1 ** var_2
+print(var_9)
+print(var_1 ** var_2)
+
+#square root
+#import math
+#var_10 = 100
+#math.sqrt(var_10)
+
