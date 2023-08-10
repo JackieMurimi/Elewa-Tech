6077e8ce1d4561b01a6110284c7a5eda5b0f3907
JackieMurimi <murimijacque@gmail.com>
2023-08-10 11:56:01 +0300

    comparison ops assignment

---
+++ comparison_ops.py
@@ -0,0 +1,56 @@
+#comparison operators
+
+a = 45
+b = 56
+c = 78
+
+#equal to
+#==
+var_1 = (a == b)
+print(var_1)
+
+var_2 = (b == c)
+print(var_2)
+
+#greater than
+#>
+var_g = (a > b)
+var_g1 = (b > c)
+var_g2 = (c > a)
+print(var_g)
+print(var_g1)
+print(var_g2)
+
+#less than
+#<
+var_l = (a < b)
+var_l1 = (b < c)
+var_l2 = (c < a)
+print(var_l)
+print(var_l1)
+print(var_l2)
+
+#not equal to
+var_n = (a != b)
+var_n1 = (b != c)
+var_n3 = (c != a)
+print(var_n)
+print(var_n1)
+print(var_n3)
+
+#less than or equal to
+var_r = (a <= b)
+var_r1 = (b <= c)
+var_r3 = (c <= a)
+print(var_r)
+print(var_r1)
+print(var_r3)
+
+#greater than or equal to
+var_s = (a >= b)
+var_s1 = (b >= c)
+var_s3 = (c >= a)
+print(var_s)
+print(var_s1)
+print(var_s3)
+
---
+++ comparison_ops_assignment.py
@@ -0,0 +1,58 @@
+#7+5 >= 10
+a = 7
+b = 5
+c = 10
+var_1 = a + b
+var_ans = var_1 >= c
+print(var_ans)
+
+#“Anil” > “anil”
+char_1 = 'Anil'
+char_2 = 'anil'
+var_ans1 = char_1 > char_2
+print(var_ans1)
+
+#25 % 3 == 2
+d = 25
+e = 3
+f = 2
+var_2 = d % e
+var_ans2 = var_2 == f
+print(var_ans2)
+
+#23 // 7  >= 9
+g = 23
+h = 7
+i = 9
+var_3 = g // h
+var_ans3 = var_3 >= i
+print(var_ans3)
+
+#7 + 3 % 2 != True
+j = 7
+k = 3
+l = 2
+char_3 = 'True'
+var_4 = j + k % l
+var_ans4 = var_4 != char_3
+print(var_ans4)
+
+#False == False
+char_4 = 'False'
+var_ans5 = char_4 == char_4
+print(var_ans5)
+
+#True != False
+char_5 = 'True'
+char_6 = 'False'
+var_ans6 = char_5 != char_6
+print(var_ans6)
+
+#29 == 89 % 30
+m = 29
+n = 89
+o = 30
+var_5 = n % o
+var_ans7 = m == var_5
+print(var_ans7)
+
