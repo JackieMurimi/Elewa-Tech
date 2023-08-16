df96c78857a946571a08ab75e1181a694106d889
JackieMurimi <murimijacque@gmail.com>
2023-08-16 21:06:58 +0300

    if_statement

---
+++ inputFunctions&Prompts/if statements/if_statement.py
@@ -0,0 +1,22 @@
+#simple if statement
+#example 1
+var_age = 16
+
+if (var_age >= 18):
+    print("Allow to the party")
+print("Go home you are underage")
+
+#example two
+var_cloth = "cotton"
+
+if (var_cloth == "cotton"):
+    print("This is the right fabric")
+print("The fabric is too soft")
+
+#example 3
+var_cam = "canon m50"
+
+if (var_cam == "Sony_cam"):
+    print("Do not purchase")
+print("purchase")
+
