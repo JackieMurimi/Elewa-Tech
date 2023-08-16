0c128a38ec202636fd159ac2c28db86cd8082556
JackieMurimi <murimijacque@gmail.com>
2023-08-16 21:06:05 +0300

    if_statement

---
+++ inputFunctions&Prompts/if statements/if_else.py
@@ -0,0 +1,30 @@
+#if else statements
+
+#school scenario
+math = 78
+physics = 80
+eng = 73
+chem = 82
+marks = (math + physics + eng + chem)
+
+if(marks >= 280):
+    print(math)
+    print(physics)
+else:
+    print(eng)
+
+#house hunting scenario
+house_a = 10000
+house_b = 15000
+budget = 12000
+
+if(budget <= 12000):
+    print(house_a)
+    print("The rent is within the budget")
+else:
+    print(house_b)
+    print("The rent is outside your budget")
+
+#
+
+
