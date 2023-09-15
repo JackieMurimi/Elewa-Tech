3b01af1b6206713727e40724dd60bb1d1b5fb994
JackieMurimi <murimijacque@gmail.com>
2023-09-15 20:50:02 +0300

    commit 1

---
+++ oop_basics.py
@@ -0,0 +1,63 @@
+#firsst oop attempt
+#we'll explore classes
+
+#syntax eg
+#create a class holding human attributes
+
+class human:
+    #features
+    feature1 = "legs"
+    feature2 = "eyes"
+    feature3 = "breasts"
+    feature4 = "dimples"
+
+#creating an object
+Anne = human()
+print(Anne.feature1)
+
+#create class chicken
+class chicken:
+    #features
+    feature_1 = "feathers"
+    feature_2 = "Thighs"
+    feature_3 = "wings"
+    feature_4 = "beak"
+
+    #chicken activities
+    act1 = "lay eggs"
+    act2 = "crow"
+    act3 = "sleep"
+
+    #create a chicken object
+chick = chicken()
+print(chick.act1)
+
+
+#class car
+class car:
+    #car parts
+    part1 = "Dashboard"
+    part2 = "Wheels"
+    part3 = "hood"
+    part4 = "bumper"
+    part5 = "brake shoe"
+    part6 = "Engine"
+
+    #car models
+    mod1 = "audi"
+    mod2 = "benz"
+    mod3 = "Toyota"
+    mod4 = "volvo"
+    mod5 = "ford"
+    mod6 = "bentley"
+
+    #object creation
+car_mine = car()
+print(car_mine.part2)
+print(car_mine.mod4)
+
+
+
+
+
+
