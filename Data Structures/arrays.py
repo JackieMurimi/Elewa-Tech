fef44aa5b43056a7ae455d811388fbf3cc4195f1
JackieMurimi <murimijacque@gmail.com>
2023-10-02 21:31:43 +0300

    arrays

---
+++ Data Structures/arrays.py
@@ -0,0 +1,13 @@
+# We are practicing the use of arrays in data structures
+# Arrays store data that is of one specific data types
+# the syntax
+'''
+array_name = arr.array(data_type, {array})
+'''
+# code starts here
+import array as r
+arrayA = r.array("i")
+# print(arrayA)
+
+arrayB = r.array("i", [24, 6, 21])
+print(arrayB)
